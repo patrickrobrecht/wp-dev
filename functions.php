b1e9505085acb8cb97719dcf55703f2d35cc51e0
Patrick Robrecht <mail@patrick-robrecht.de>
2016-10-10 22:28:19 +0200

    Cache json files for max 24h

--- functions.php
+++ functions.php
@@ -12,13 +12,12 @@ function get_update_message( $type, $plugin, $copied ) {
 
 function get_plugin_file( $plugin, $update = false ) {
 	$file_cache = 'data/plugins/' . $plugin . '.json';
-	if ( $update || !file_exists( $file_cache ) ) {
+	if ( $update || !file_exists( $file_cache ) || is_old( $file_cache ) ) {
 		$api_url = sprintf(
 				'https://api.wordpress.org/plugins/info/1.0/%s.json',
 				$plugin
 		);
 		$copied = copy( $api_url, $file_cache );
-		
 		echo get_update_message( 'plugin', $plugin, $copied );
 	}
 	return file_get_contents( $file_cache );
@@ -26,7 +25,7 @@ function get_plugin_file( $plugin, $update = false ) {
 
 function get_plugin_stats_file( $plugin, $update = false ) {
 	$file_cache = 'data/plugins-stats/' . $plugin . '.json';
-	if ( $update || !file_exists( $file_cache ) ) {
+	if ( $update || !file_exists( $file_cache ) || is_old( $file_cache ) ) {
 		$api_url = sprintf(
 				'https://api.wordpress.org/stats/plugin/1.0/%s',
 				$plugin
@@ -39,7 +38,7 @@ function get_plugin_stats_file( $plugin, $update = false ) {
 
 function get_plugin_translations_file( $plugin, $update = false ) {
 	$file_cache = 'data/plugins-translations/' . $plugin . '.json';
-	if ( $update || !file_exists( $file_cache ) ) {
+	if ( $update || !file_exists( $file_cache ) || is_old( $file_cache ) ) {
 		$api_url = sprintf(
 				'https://api.wordpress.org/translations/plugins/1.0/?slug=%s',
 				$plugin
@@ -58,4 +57,9 @@ function get_duration( $start_time ) {
 
 function get_duration_for_output( $start_time, $text = 'Executed in %s seconds' ) {
 	return sprintf( $text, number_format( get_duration( $start_time ), 2 ) );
+}
+
+function is_old( $file ) {
+	$lastchanged = filectime ( $file );
+	return ( date( 'U' ) - $lastchanged ) > ( 24 * 60 * 60 );
 }
